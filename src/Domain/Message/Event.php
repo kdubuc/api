f7fb06e1349cdac5e269e6f2ca71b7c9e9519460
Kévin DUBUC <kevindubuc62@gmail.com>
2017-05-11 09:46:34 +0200

    SimpleEventStore added (use DBAL/Connection)

--- src/Domain/Message/Event.php
+++ src/Domain/Message/Event.php
@@ -2,6 +2,9 @@
 
 namespace API\Domain\Message;
 
+use ReflectionClass;
+use Datetime;
+use Exception;
 use API\Domain\ValueObject\ID;
 use API\Message\Event\Event as BaseEvent;
 
@@ -16,8 +19,8 @@ abstract class Event extends BaseEvent
      */
     public function setEmitterId(ID $emitter_id) : Event
     {
-        if (!empty($this->id)) {
-            throw new Exception('Change emitter id is not allowed', 1);
+        if (!empty($this->emitter_id)) {
+            throw new Exception('Change emitter id is not allowed');
         }
 
         $this->emitter_id = $emitter_id;
@@ -28,10 +31,40 @@ abstract class Event extends BaseEvent
     /**
      * Get the emitter id.
      *
-     * @return string
+     * @return API\Domain\ValueObject\ID
      */
     public function getEmitterId() : ID
     {
         return $this->emitter_id;
+    }
+
+    /**
+     * Rebuild event from the record date, and payload.
+     *
+     * @param API\Domain\Message\Event
+     *
+     * @return API\Domain\Message\Event
+     */
+    public static function rebuild(string $event_id, ID $emitter_id, Datetime $record_date, array $payload = []) : Event
+    {
+        $current_class = get_called_class();
+
+        $reflection = new ReflectionClass($current_class);
+
+        $event = $reflection->newInstanceWithoutConstructor();
+
+        $event_id = $reflection->getProperty('id');
+        $event_id->setAccessible(true);
+        $event_id->setValue($event, $event_id);
+
+        $event_record_date = $reflection->getProperty('record_date');
+        $event_record_date->setAccessible(true);
+        $event_record_date->setValue($event, $record_date);
+
+        $event->setEmitterId($emitter_id);
+
+        $event->fillPayload($payload);
+
+        return $event;
     }
 }
